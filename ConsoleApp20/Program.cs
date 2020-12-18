a05e16709403011acc666560f6abc78d3c713ab0
fsafdsa Lfybkf <P.danila228@mail.ru>
2020-12-18 17:29:37 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("entet a:");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("enter b:");
+                int b = int.Parse(Console.ReadLine());
+                Console.Write("enter c:");
+                int c = int.Parse(Console.ReadLine());
+                bool x = (a < 45) ^ (b < 45) ^ (c < 45);
+
+                Console.WriteLine(x);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+    
+
